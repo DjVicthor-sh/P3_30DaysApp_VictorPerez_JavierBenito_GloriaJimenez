425186679c9fb0106da52c0fc6bd55023781a9b7
Victhor <victor.perezcano01@proton.me>
2025-12-16 10:31:53 +0100

    Initial commit

---
+++ app/src/main/java/com/example/p3_30daysapp_victorperez_javierbenito_gloriajimenez/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.p3_30daysapp_victorperez_javierbenito_gloriajimenez
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.p3_30daysapp_victorperez_javierbenito_gloriajimenez.ui.theme.P3_30DaysApp_VictorPerez_JavierBenito_GloriaJimenezTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            P3_30DaysApp_VictorPerez_JavierBenito_GloriaJimenezTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    P3_30DaysApp_VictorPerez_JavierBenito_GloriaJimenezTheme {
+        Greeting("Android")
+    }
+}
