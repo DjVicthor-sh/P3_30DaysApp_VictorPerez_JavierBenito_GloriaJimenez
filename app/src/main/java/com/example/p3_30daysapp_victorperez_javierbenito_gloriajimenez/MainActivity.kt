5f32ed6d64f84522f959e8d949c0bb9027165803
Victhor <victor.perezcano01@proton.me>
2026-01-05 17:55:41 +0100

    NavigationBar

--- app/src/main/java/com/example/p3_30daysapp_victorperez_javierbenito_gloriajimenez/MainActivity.kt
+++ app/src/main/java/com/example/p3_30daysapp_victorperez_javierbenito_gloriajimenez/MainActivity.kt
@@ -13,12 +13,20 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -58,16 +66,52 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun DaysListApp() {
     Surface(
-        modifier = Modifier.padding(top = 24.dp, bottom = 24.dp)
+        modifier = Modifier
     ) {
         Column() {
+            Column(modifier = Modifier
+                .padding(top = 24.dp)
 
-            LazyColumn(modifier = Modifier.padding(16.dp)) {
-                items(DaySource.days) { day ->
-                    DayCard(day = day, modifier = Modifier.padding(bottom = 16.dp))
+            ) {
+
+                NavigationBar(
+                    modifier = Modifier
+
+
+                ) {
+                    NavigationBarItem(
+                        icon = {
+                            Icon(
+                                imageVector = Icons.Default.Menu,
+                                contentDescription = null
+                            )
+                        },
+                        selected = true,
+                        onClick = {}
+                    )
+                    NavigationBarItem(
+                        icon = {
+                            Icon(
+                                imageVector = Icons.Default.AccountCircle,
+                                contentDescription = null
+                            )
+                        },
+                        selected = true,
+                        onClick = {}
+                    )
+
+                }
+            }
+            Column(modifier = Modifier) {
+
+                LazyColumn(modifier = Modifier.padding(16.dp)) {
+                    items(DaySource.days) { day ->
+                        DayCard(day = day, modifier = Modifier.padding(bottom = 16.dp))
+                    }
                 }
             }
         }
+
     }
 }
 
