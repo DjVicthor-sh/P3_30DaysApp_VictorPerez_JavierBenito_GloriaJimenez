905968294446043545728263272777cb31805e70
camel135 <damir.jabe@gmail.com>
2026-01-08 12:27:39 +0100

    Imagenes y descripciones Perros

--- app/src/main/java/com/example/p3_30daysapp_victorperez_javierbenito_gloriajimenez/data/DaySource.kt
+++ app/src/main/java/com/example/p3_30daysapp_victorperez_javierbenito_gloriajimenez/data/DaySource.kt
@@ -2,39 +2,39 @@ package com.example.p3_30daysapp_victorperez_javierbenito_gloriajimenez.data
 
 import com.example.p3_30daysapp_victorperez_javierbenito_gloriajimenez.R
 import com.example.p3_30daysapp_victorperez_javierbenito_gloriajimenez.model.Day
-//es la lista que contiene los 30 dias
+
+// es la lista que contiene los 30 dias
 object DaySource {
     val days = listOf(
-        Day(R.string.animal_01, 1,  R.drawable.animal_1, R.string.descAnimal_01),
-        Day(R.string.animal_02, 2,  R.drawable.animal_2, R.string.descAnimal_01),
-        Day(R.string.animal_03, 3,  R.drawable.animal_3, R.string.descAnimal_01),
-        Day(R.string.animal_04, 4, R.drawable.animal_4, R.string.descAnimal_01),
-        Day(R.string.animal_05, 5, R.drawable.animal_5, R.string.descAnimal_01),
-        Day(R.string.animal_06, 6, R.drawable.animal_6, R.string.descAnimal_01),
-        Day(R.string.animal_07, 7, R.drawable.animal_7, R.string.descAnimal_01),
-        Day(R.string.animal_08, 8, R.drawable.animal_8, R.string.descAnimal_01),
-        Day(R.string.animal_09, 9, R.drawable.animal_9, R.string.descAnimal_01),
-        Day(R.string.animal_10, 10, R.drawable.animal_10, R.string.descAnimal_01),
-        Day(R.string.animal_11, 11, R.drawable.animal_11, R.string.descAnimal_01),
-        Day(R.string.animal_12, 12, R.drawable.animal_12, R.string.descAnimal_01),
-        Day(R.string.animal_13, 13, R.drawable.animal_13, R.string.descAnimal_01),
-        Day(R.string.animal_14, 14, R.drawable.animal_14, R.string.descAnimal_01),
-        Day(R.string.animal_15, 15, R.drawable.animal_15, R.string.descAnimal_01),
-        Day(R.string.animal_16, 16, R.drawable.animal_16, R.string.descAnimal_01),
-        Day(R.string.animal_17, 17, R.drawable.animal_17, R.string.descAnimal_01),
-        Day(R.string.animal_18, 18, R.drawable.animal_18, R.string.descAnimal_01),
-        Day(R.string.animal_19, 19, R.drawable.animal_19, R.string.descAnimal_01),
-        Day(R.string.animal_20, 20, R.drawable.animal_20, R.string.descAnimal_01),
-        Day(R.string.animal_21, 21, R.drawable.animal_21, R.string.descAnimal_01),
-        Day(R.string.animal_22, 22, R.drawable.animal_22, R.string.descAnimal_01),
-        Day(R.string.animal_23, 23, R.drawable.animal_23, R.string.descAnimal_01),
-        Day(R.string.animal_24, 24, R.drawable.animal_24, R.string.descAnimal_01),
-        Day(R.string.animal_25, 25, R.drawable.animal_25, R.string.descAnimal_01),
-        Day(R.string.animal_26, 26, R.drawable.animal_26, R.string.descAnimal_01),
-        Day(R.string.animal_27, 27, R.drawable.animal_27, R.string.descAnimal_01),
-        Day(R.string.animal_28, 28, R.drawable.animal_28, R.string.descAnimal_01),
-        Day(R.string.animal_29, 29, R.drawable.animal_29, R.string.descAnimal_01),
-        Day(R.string.animal_30, 30, R.drawable.animal_30, R.string.descAnimal_01),
-)
-
+        Day(R.string.animal_01, 1, R.drawable.perro_01, R.string.descAnimal_01),
+        Day(R.string.animal_02, 2, R.drawable.perro_02, R.string.descAnimal_02),
+        Day(R.string.animal_03, 3, R.drawable.perro_03, R.string.descAnimal_03),
+        Day(R.string.animal_04, 4, R.drawable.perro_04, R.string.descAnimal_04),
+        Day(R.string.animal_05, 5, R.drawable.perro_05, R.string.descAnimal_05),
+        Day(R.string.animal_06, 6, R.drawable.perro_06, R.string.descAnimal_06),
+        Day(R.string.animal_07, 7, R.drawable.perro_07, R.string.descAnimal_07),
+        Day(R.string.animal_08, 8, R.drawable.perro_08, R.string.descAnimal_08),
+        Day(R.string.animal_09, 9, R.drawable.perro_09, R.string.descAnimal_09),
+        Day(R.string.animal_10, 10, R.drawable.perro_10, R.string.descAnimal_10),
+        Day(R.string.animal_11, 11, R.drawable.perro_11, R.string.descAnimal_11),
+        Day(R.string.animal_12, 12, R.drawable.perro_12, R.string.descAnimal_12),
+        Day(R.string.animal_13, 13, R.drawable.perro_13, R.string.descAnimal_13),
+        Day(R.string.animal_14, 14, R.drawable.perro_14, R.string.descAnimal_14),
+        Day(R.string.animal_15, 15, R.drawable.perro_15, R.string.descAnimal_15),
+        Day(R.string.animal_16, 16, R.drawable.perro_16, R.string.descAnimal_16),
+        Day(R.string.animal_17, 17, R.drawable.perro_17, R.string.descAnimal_17),
+        Day(R.string.animal_18, 18, R.drawable.perro_18, R.string.descAnimal_18),
+        Day(R.string.animal_19, 19, R.drawable.perro_19, R.string.descAnimal_19),
+        Day(R.string.animal_20, 20, R.drawable.perro_20, R.string.descAnimal_20),
+        Day(R.string.animal_21, 21, R.drawable.perro_21, R.string.descAnimal_21),
+        Day(R.string.animal_22, 22, R.drawable.perro_22, R.string.descAnimal_22),
+        Day(R.string.animal_23, 23, R.drawable.perro_23, R.string.descAnimal_23),
+        Day(R.string.animal_24, 24, R.drawable.perro_24, R.string.descAnimal_24),
+        Day(R.string.animal_25, 25, R.drawable.perro_25, R.string.descAnimal_25),
+        Day(R.string.animal_26, 26, R.drawable.perro_26, R.string.descAnimal_26),
+        Day(R.string.animal_27, 27, R.drawable.perro_27, R.string.descAnimal_27),
+        Day(R.string.animal_28, 28, R.drawable.perro_28, R.string.descAnimal_28),
+        Day(R.string.animal_29, 29, R.drawable.perro_29, R.string.descAnimal_29),
+        Day(R.string.animal_30, 30, R.drawable.perro_30, R.string.descAnimal_30)
+    )
 }
